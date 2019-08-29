b39a73723d889355ecb058dae2e911c0dcc1a05e
Luca Steeb <contact@luca-steeb.com>
2019-08-29 17:23:25 +0200

    feat(spawn): log time taken

--- spawn.sh
+++ spawn.sh
@@ -4,11 +4,18 @@
 
 log="logs/$(echo $i | tr '/' '-').txt"
 
+start=$(date +%s.%N)
+
 set +e
 make i=$i run > $log 2>&1
+ret=$?
+set -e
 
-if [ $? -eq 0 ]; then
-  echo "$i $(tput setaf 2)success$(tput sgr0)"
+end=$(date +%s.%N)
+diff=$(printf '%.*fs' 1 $(echo "$end - $start" | bc))
+
+if [ $ret -eq 0 ]; then
+  echo "$i $(tput setaf 2)success$(tput sgr0) $diff"
 else
-  echo "$i $(tput setaf 1)fail$(tput sgr0); please see file://$(pwd)/$log for details"
+  echo "$i $(tput setaf 1)fail$(tput sgr0) $diff; please see file://$(pwd)/$log for details"
 fi
