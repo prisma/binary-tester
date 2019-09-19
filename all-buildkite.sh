bb992e91fdce79555953287fd369c315a103156c
Luca Steeb <contact@luca-steeb.com>
2019-09-19 15:08:35 +0200

    ci(buildkite): run raw tests in addition to fetch-engine tests

--- all-buildkite.sh
+++ all-buildkite.sh
@@ -6,7 +6,8 @@ set -eu
 
 cmd=""
 
-while read item; do
+# raw test first
+while read -r item; do
   i=$(echo $item | cut -d# -f1 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
   note=$(echo $item | cut -d# -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 
@@ -19,9 +20,31 @@ while read item; do
   fi
 
   cmd="$cmd
-  - label: '$note'
+  - label: '(direct) $note'
     command: make i='$i' test
     soft_fail: true"
-done <all.txt
+done <images.txt
+
+cmd="$cmd
+  - wait"
+
+# run full tests with fetch-engine
+while read -r item; do
+  i=$(echo $item | cut -d# -f1 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+  note=$(echo $item | cut -d# -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+  if [ "$i" = "" ]; then
+    continue
+  fi
+
+  if [ "$note" = "" ]; then
+    note="$i"
+  fi
+
+  cmd="$cmd
+  - label: '(fetch-engine) $note'
+    command: make i='$i' raw-test
+    soft_fail: true"
+done <images.txt
 
 echo "steps: $cmd"
