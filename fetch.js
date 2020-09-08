ecb7ce0c8672b1246ad946eeb26e05415a7a276d
Joël <joel.galeran@gmail.com>
2020-09-08 10:10:53 +0200

    feat: add introspection-engine & prisma-fmt to fetch script

--- fetch.js
+++ fetch.js
@@ -1,34 +1,38 @@
-const { download } = require('@prisma/fetch-engine')
+const { download } = require("@prisma/fetch-engine");
 
-process.env.DEBUG = '*'
-const version = process.env.FETCH_ENGINE_VERSION || 'latest'
+process.env.DEBUG = "*";
+const version = process.env.FETCH_ENGINE_VERSION || "latest";
 
-console.log('using @prisma/fetch-engine version', version)
+console.log("using @prisma/fetch-engine version", version);
 
-let last = 0
+let last = 0;
 
 download({
-	binaries: {
-		'query-engine': '.',
-		'migration-engine': '.',
-	},
-	showProgress: false,
-	progressCb: onProgress,
-	version,
-}).then(() => {
-  console.log('download successful')
-}).catch((err) => {
-  console.log('download failed:')
-  console.error(err)
-  process.exit(1)
+  binaries: {
+    "query-engine": ".",
+    "migration-engine": ".",
+    "introspection-engine": ".",
+    "prisma-fmt": ".",
+  },
+  showProgress: false,
+  progressCb: onProgress,
+  version,
 })
+  .then(() => {
+    console.log("download successful");
+  })
+  .catch((err) => {
+    console.log("download failed:");
+    console.error(err);
+    process.exit(1);
+  });
 
 // onProgress rounds float n to a percentage % 10
 function onProgress(n) {
   // round by one digit (0.1234 -> 0.1) and display
-  const f = Math.round(n * 10) / 10
+  const f = Math.round(n * 10) / 10;
   if (last !== f) {
-    console.log((f * 100) + "%") // e.g. renders 10%
+    console.log(f * 100 + "%"); // e.g. renders 10%
   }
-  last = f
+  last = f;
 }
